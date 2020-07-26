b158c078f1b874c0b481a620889952c248e9ee2b
Marcel <flashdagger@googlemail.com>
2020-07-26 17:04:49 +0200

    cleanup.py - remove download caches and show freed space

--- conmon/cleanup.py
+++ conmon/cleanup.py
@@ -5,10 +5,11 @@ import re
 import shlex
 import shutil
 import sys
+from contextlib import suppress
 from datetime import datetime
 from pathlib import Path
-from subprocess import CalledProcessError, check_output
-from typing import List
+from subprocess import CalledProcessError, PIPE, check_output
+from typing import List, Optional
 
 import colorama  # type: ignore
 import colorlog  # type: ignore
@@ -16,8 +17,32 @@ import colorlog  # type: ignore
 LOG = logging.getLogger("CLEANUP")
 
 
-def conan(cmd: str) -> str:
-    output = check_output(["conan", *shlex.split(cmd)], universal_newlines=True)
+# pylint: disable=too-few-public-methods
+class GLOBALS:
+    now = datetime.now()
+    debug = False
+
+
+def human_readable_size(size: int, precision: int = 1) -> str:
+    ssize = float(size)
+    for dim in ("Bytes", "kB", "MB", "GB"):
+        if ssize > 1024:
+            ssize = ssize / 1024
+        else:
+            break
+
+    fsize = f"{ssize:.{precision}f}"
+    return f"{fsize.rstrip('.0')} {dim}"
+
+
+def conan(cmd: str) -> Optional[str]:
+    try:
+        output = check_output(
+            ["conan", *shlex.split(cmd)], stderr=PIPE, universal_newlines=True
+        )
+    except CalledProcessError as exc:
+        LOG.warning(exc.stderr.rstrip())
+        return None
     return output.rstrip()
 
 
@@ -29,15 +54,26 @@ def ref_from_path(path: Path) -> str:
     return ref
 
 
+def folder_size(path: Path) -> int:
+    total = 0
+    for file in path.glob("**/*"):
+        if not file.is_file():
+            continue
+        total += file.stat().st_size
+    return total
+
+
 def remove_path(path: Path):
     shutil.rmtree(path, ignore_errors=True)
 
 
-def cleanup_conan(args) -> int:
-    cache = Path(conan("config get storage.path"))
-    now = datetime.now()
+def cleanup_conan_cache(args) -> int:
+    config_cache = conan("config get storage.path")
+    if config_cache is None:
+        return 1
+    cache = Path(config_cache)
     regex = re.compile(args.filter)
-    any_match = False
+    total_size = 0
     return_status = 0
 
     for path in cache.glob("*/*/*/*"):
@@ -50,33 +86,73 @@ def cleanup_conan(args) -> int:
 
         stat = conan_file.stat()
         ref = ref_from_path(path)
-        age = now - datetime.fromtimestamp(stat.st_atime)
-        if args.debug:
+        age = GLOBALS.now - datetime.fromtimestamp(stat.st_atime)
+        if GLOBALS.debug:
             LOG.debug("%s age: %s days", ref, age.days)
 
         if age.days <= args.days or not regex.match(ref):
             continue
 
-        any_match = True
+        fsize = folder_size(path)
+        hr_size = human_readable_size(fsize)
+        info = f"{hr_size}, {age.days} days"
         if args.dry_run:
-            LOG.info("Would delete %s (%s days)", ref, age.days)
+            LOG.info("Would delete %s (%s)", ref, info)
+            total_size += fsize
         else:
-            LOG.info("Deleting %s (%s days)", ref, age.days)
-            try:
-                conan(f"remove --force {ref}")
-            except CalledProcessError as exc:
-                if args.debug:
-                    LOG.debug(exc)
+            LOG.info("Deleting %s (%s days)", ref, info)
+            if conan(f"remove --force {ref}") is None:
                 return_status = 1
+            else:
+                total_size += fsize
 
-    if not any_match:
+    if total_size == 0:
         LOG.info("Nothing to delete.")
+    else:
+        LOG.info("Freed %s in conan cache.", human_readable_size(total_size))
 
     return return_status
 
 
+def cleanup_conan_dlcache(args):
+    output = conan("config get storage.download_cache")
+    if output is None:
+        return
+
+    cache = Path(output)
+    total_size = 0
+    for path in cache.iterdir():
+        if not path.is_file():
+            continue
+        stat = path.stat()
+        age = GLOBALS.now - datetime.fromtimestamp(stat.st_atime)
+        if age.days <= args.days:
+            continue
+        size = stat.st_size
+        hr_size = human_readable_size(size)
+        info = f"{hr_size}, {age.days} days"
+        if args.dry_run:
+            LOG.info("Would delete %s (%s)", path.name, info)
+            total_size += size
+        else:
+            LOG.info("Deleting %s (%s)", path.name, info)
+            with suppress(OSError):
+                path.unlink()
+                total_size += size
+                (path / "locks" / path.name).unlink(missing_ok=True)
+
+    if total_size > 0:
+        LOG.info("Freed %s in download cache.", human_readable_size(total_size))
+
+
+def cleanup_conan(args) -> int:
+    cleanup_conan_dlcache(args)
+    return cleanup_conan_cache(args)
+
+
 def main() -> int:
     args = parse_args(sys.argv[1:])
+    GLOBALS.debug = args.debug
 
     colorama_args = dict(autoreset=True, convert=None, strip=None, wrap=True)
     # prevent messing up colorama settings
