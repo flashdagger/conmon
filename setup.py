7bcfaa935976231de61a19a27db1f1d05ba1c61a
Marcel <flashdagger@googlemail.com>
2022-03-27 07:38:25 +0200

    move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -3,40 +3,4 @@
 
 from setuptools import setup
 
-project = "conmon"
-
-setup(
-    name=project,
-    version="0.1.0",
-    description="Run conan as monitored process.",
-    long_description="Run conan as monitored process.",
-    author="flashdagger",
-    author_email="flashdagger@googlemail.com",
-    license="MIT",
-    url="https://github.com/flashdagger/conmon",
-    platforms="any",
-    packages=[project],
-    entry_points={
-        "console_scripts": [
-            "conmon=conmon.__main__:main",
-            "conmon-clean-cache=conmon.cleanup:main",
-        ]
-    },
-    include_package_data=True,
-    # project dependencies for installation
-    python_requires=">=3.6",
-    install_requires=["psutil>=5.7", "colorama>=0.3.3", "colorlog>=4.1"],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    test_suite="tests",
-    zip_safe=False,
-    keywords="",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-    ],
-)
+setup()
